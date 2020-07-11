2a60dcb056d4d42d6cf1b98dbaab970633906a7f
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-11 13:53:12 +0900

    [add]index.showアクション追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,10 @@
 class BooksController < ApplicationController
   def index
+  	@books = Book.all
   end
 
   def show
+  	@book = Book.find(params[:id])
   end
 
   def new
@@ -12,10 +14,17 @@ class BooksController < ApplicationController
   def create
   	book = Book.new(book_params)
   	book.save
-  	redirect_to books_path
+  	redirect_to book_path(book.id)
   end
 
   def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	book = Book.find(params[:id])
+  	book.update(book_params)
+  	redirect_to book_path(book)
   end
 
   private
