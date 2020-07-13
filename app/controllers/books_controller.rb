aece2edbfcf4915bcb7fb4395c674377d0c0ab0d
shofujimoto0617 <shofujimoto017@gmail.com>
2020-07-13 14:38:47 +0900

    [add]エラー文表示設定

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,15 +8,15 @@ class BooksController < ApplicationController
   	@book = Book.find(params[:id])
   end
 
-  def new
-  	@book = Book.new
-  end
-
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	flash[:notice] = "Book was successfully created."
-  	redirect_to book_path(book.id)
+  	@book = Book.new(book_params)
+  	@books = Book.all
+  	if @book.save
+  	  flash[:notice] = "Book was successfully created."
+  	  redirect_to book_path(@book.id)
+    else
+      render :index
+  	end
   end
 
   def edit
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
   def update
   	book = Book.find(params[:id])
   	book.update(book_params)
+  	flash[:notice] = "Book was successfully updated."
   	redirect_to book_path(book)
   end
 
